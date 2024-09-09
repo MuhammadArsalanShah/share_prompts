dd00409b61568cf216c742247118192b2cb4b155
MuhammadArsalanShah <arsalanshah408@yahoo.com>
2024-09-09 13:26:06 +0500

    Creating routes in Next.js. Next.js has file-based routing

---
+++ app/fonts/posts/new/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// will create route
+// localhost:3000/posts
+// Or we can say
+// /posts/new
+
+const page = () => {
+  return (
+    <div>NEW POST</div>
+  )
+}
+
+export default page
---
+++ app/fonts/posts/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// will create route
+// localhost:3000/posts
+// Or we can say
+// /posts
+
+const page = () => {
+  return (
+    <div>POSTS</div>
+  )
+}
+
+export default page
